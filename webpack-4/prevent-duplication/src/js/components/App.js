8625e32355491c5623f32e90ddb4f3fb1d484fec
Jhon Guerrero <jhoguer@gmail.com>
2020-03-08 18:02:01 -0500

    Evitar código duplicado

---
+++ webpack-4/prevent-duplication/src/js/components/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './Loader';
+import logo from '../../images/platzi.png';
+import video from '../../video/que-es-core.mp4';
+
+import '../../sass/sass.scss';
+import '../../less/less.less';
+import '../../stylus/stylus.styl';
+
+console.log(data)
+
+const App = () => {
+  const [loaderList, setLoaderList] = useState([]);
+  const handleClick = () => {
+    console.log(setLoaderList(data.loaders))
+  }
+  
+  return (
+    <div>
+      <p className="sass">
+        esto es sass
+      </p>
+      <p className="less">
+        esto es less
+      </p>
+      <p className="stylus">
+        esto es stylus
+      </p>
+      <p className="post-css">
+        esto es postcss
+      </p>
+      Que linda aplicacion hecha en React.js
+      <video src={video} width={360} controls poster={logo}></video>
+      <p>
+        <img src={logo} alt="" width={40} />
+      </p>
+      <ul>
+        {
+          loaderList.map(item => <Loader {...item} key={item.id} /> )
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack-4/prevent-duplication/src/js/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+  document.body.innerHTML = '<p>Hola mundo con WebPack 4!! en </p>';
+}
+
+export default text;
